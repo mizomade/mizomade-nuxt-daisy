f5de3cc3aa51f2720b9d69adebd6c04d4965bbcd
mizomade <mizomade.official@gmail.com>
2022-06-28 02:04:13 +0530

    update API

--- API.js
+++ API.js
@@ -1,8 +1,8 @@
 // const  API_URL = "http://192.168.43.242:8000";
 // const  API_URL = "http://localhost:8000";
-const  API_URL = process.env.API_URL;
+const  API_URL = "https://mizomade.pythonanywhere.com";
 
 
 
-const MEDIA_S3= process.env.MEDIA_S3;
+const MEDIA_S3= 'https://mizomade-media-buckets.s3.ap-south-1.amazonaws.com/';
 export {API_URL,MEDIA_S3};
